d30aeb76b2a5e021340aceabbbf9c8e999a5d782
Lucien Mary <38400510+lucienmary@users.noreply.github.com>
2022-09-08 22:32:11 +0200

    Basis of the init function + Structure of the navigation in the view

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ $(function() {
     const CTX = $('#ctx');
     const GLASS = $('#glass');
     const ASSETS_CONTAINER = $('#assets');
+    
+    const COMMAND = {
+        top: $('.arrow-top'), // useless by default.
+        right: $('.arrow-right'),
+        bottom: $('.arrow-bottom'), // useless by default.
+        left: $('.arrow-left')
+    }
 
     const SETTINGS = {
-        zoom: 0.66666666
+        zoom: 0.66666666,
+        axis: 'xy' // x, y, xy.
     }
 
     function init(params) {
@@ -17,8 +25,41 @@ $(function() {
 
         CTX.css({'width' : CTX.width, 'height' : CTX.height});
         GLASS.css({'width' : CTX.width, 'height' : CTX.height});
+
+        switch (SETTINGS.axis) {
+            case 'x':
+                COMMAND.right.css('display', 'unset');
+                COMMAND.left.css('display', 'unset');
+                break;
+            case 'y':
+                COMMAND.top.css('display', 'unset');
+                COMMAND.bottom.css('display', 'unset');
+                break;
+            case 'xy':
+                COMMAND.top.css('display', 'unset').addClass('arrow-y-top-tiny');
+                COMMAND.right.css('display', 'unset');
+                COMMAND.bottom.css('display', 'unset').addClass('arrow-y-bottom-tiny');
+                COMMAND.left.css('display', 'unset');
+                break;
+            default:
+                COMMAND.right.css('display', 'unset');
+                COMMAND.left.css('display', 'unset');
+
+                console.error('SETTINGS.axis is not defined. X axis is by default at init function.');
+                SETTINGS.axis = 'x';
+                break;
+        }
     }
 
     init();
+
+    GLASS.on('click', (e) => {
+        move(e);
+    });
+
+    function move(e) {
+        const DIRECTION = $(e.target).data('direction');
+        console.log(DIRECTION);
+    }
     
 });
